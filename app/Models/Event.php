e8d13d2e437deb24a349dba97b703f198006e645
Turnionz <turn0nz@gmail.com>
2026-05-05 22:51:45 +0300

    Resource classes with JSON response control

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
@@ -11,12 +13,12 @@ class Event extends Model
 
     protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function attendee()
+    public function attendees(): HasMany
     {
         return $this->hasMany(Attendee::class);
     }
