9a630185e7075b35398f8d7cd935cbe82887f675
Turnionz <turn0nz@gmail.com>
2026-05-06 17:49:38 +0300

    Trait CanLoadRelationships added

---
+++ app/Http/Traits/CanLoadRelationships.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Contracts\Database\Eloquent\Builder as EloquentBuilder;
+
+trait CanLoadRelationships
+{
+    /**
+     * loadRelationships trait for loading relationships that are available for query
+     *
+     * @param Model|QueryBuilder|EloquentBuilder $for
+     * @param array|null $relations
+     * @return Model|QueryBuilder|EloquentBuilder
+     */
+    public function loadRelationships(Model|QueryBuilder|EloquentBuilder $for, ?array $relations = null): Model|QueryBuilder|EloquentBuilder
+    {
+        $relations = $relations ?? $this->relations ?? [];
+
+        foreach ($relations as $relation) {
+            $for->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $for instanceof Model ? $for->load($relation) : $q->with($relation)
+            );
+        }
+
+        return $for;
+    }
+
+    protected function shouldIncludeRelation(string $relation)
+    {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+
+        return in_array($relation, $relations);
+    }
+}
